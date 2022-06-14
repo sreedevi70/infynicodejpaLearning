7c94107b794b77fe24d60ab2cde9bb248ab95ec1
Sreedevi <esrujan98@gmail.com>
2022-06-14 16:10:03 -0500

    new code

---
+++ src/main/java/org/jpacode/JpaLearningApplication.java
@@ -0,0 +1,13 @@
+package org.jpacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLearningApplication.class, args);
+	}
+
+}
